784861b157d585281b066651d53860eb72f39ee1
Alfonso Juarez <joses.guerreros@gmail.com>
2022-08-27 03:40:31 -0500

    Chache control

--- app/src/main/java/com/example/themoviedb/request/Services.java
+++ app/src/main/java/com/example/themoviedb/request/Services.java
@@ -1,14 +1,43 @@
 package com.example.themoviedb.request;
 
+import android.util.Log;
+
+import com.example.themoviedb.MainActivity;
 import com.example.themoviedb.utils.Credenciales;
 import com.example.themoviedb.utils.MovieApi;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class Services {
 
-    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl(Credenciales.URL_BASE).addConverterFactory(GsonConverterFactory.create());
+    private static  Services instance;
+    public static  Services getInstance(){
+        if(instance == null){
+            instance = new Services();
+        }
+        return instance;
+    }
+
+    public static final String HEADER_CACHE_CONTROL = "Cache-Control";
+    public static final String HEADER_PRAGMA = "Pragma";
+    private static final String TAG = "Cledata";
+    private static final long cacheSize = 5 *1024 * 1024; // 5 MB
+
+
+
+    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl(Credenciales.URL_BASE).client(okHttpClient()).addConverterFactory(GsonConverterFactory.create());
 
     private  static Retrofit retrofit = retrofitBuilder.build();
 
@@ -16,5 +45,87 @@ public class Services {
 
     public static MovieApi getMovieApi(){
         return movieApi;
+    }
+
+    private static OkHttpClient okHttpClient(){
+        return new OkHttpClient.Builder()
+                .cache(cache())
+                .addInterceptor(httpLoggingInterceptor()) // used if network off OR on
+                .addNetworkInterceptor(networkInterceptor()) // only used when network is on
+                .addInterceptor(offlineInterceptor())
+                .build();
+    }
+
+    private static Cache cache(){
+        return new Cache(new File(MyApplication.getInstance().getCacheDir(),"someIdentifier"), cacheSize);
+    }
+
+    /**
+     * This interceptor will be called both if the network is available and if the network is not available
+     * @return
+     */
+    private static Interceptor offlineInterceptor() {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Log.d(TAG, "offline interceptor: called.");
+                Request request = chain.request();
+
+                // prevent caching when network is on. For that we use the "networkInterceptor"
+                if (!MyApplication.hasNetwork()) {
+                    CacheControl cacheControl = new CacheControl.Builder()
+                            .maxStale(7, TimeUnit.DAYS)
+                            .build();
+
+                    request = request.newBuilder()
+                            .removeHeader(HEADER_PRAGMA)
+                            .removeHeader(HEADER_CACHE_CONTROL)
+                            .cacheControl(cacheControl)
+                            .build();
+                }
+
+                return chain.proceed(request);
+            }
+        };
+    }
+
+    /**
+     * This interceptor will be called ONLY if the network is available
+     * @return
+     */
+    private static Interceptor networkInterceptor() {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Log.d(TAG, "network interceptor: called.");
+
+                Response response = chain.proceed(chain.request());
+
+                CacheControl cacheControl = new CacheControl.Builder()
+                        .maxAge(5, TimeUnit.SECONDS)
+                        .build();
+
+                return response.newBuilder()
+                        .removeHeader(HEADER_PRAGMA)
+                        .removeHeader(HEADER_CACHE_CONTROL)
+                        .header(HEADER_CACHE_CONTROL, cacheControl.toString())
+                        .build();
+            }
+        };
+    }
+
+    private static HttpLoggingInterceptor httpLoggingInterceptor ()
+    {
+        HttpLoggingInterceptor httpLoggingInterceptor =
+                new HttpLoggingInterceptor( new HttpLoggingInterceptor.Logger()
+                {
+                    @Override
+                    public void log (String message)
+                    {
+                        Log.d(TAG, "log: http log: " + message);
+                    }
+                } );
+        httpLoggingInterceptor.setLevel( HttpLoggingInterceptor.Level.BODY);
+        return httpLoggingInterceptor;
     }
 }
